4b7dc87cced701c0e013e96928bef15d50bbc820
hlewluv <153503120+hlewluv@users.noreply.github.com>
2024-06-23 17:40:44 +0700

    Update ImageHelper.cs

--- Areas/Admin/Helper/ImageHelper.cs
+++ Areas/Admin/Helper/ImageHelper.cs
@@ -8,7 +8,22 @@ namespace CinemaWeb.Areas.Admin.Helper
 {
     public class ImageHelper
     {
-
+        public static string ConvertFileToBase64(HttpPostedFileBase file)
+        {
+            try
+            {
+                byte[] imageBytes;
+                using (BinaryReader reader = new BinaryReader(file.InputStream))
+                {
+                    imageBytes = reader.ReadBytes(file.ContentLength);
+                }
+                return Convert.ToBase64String(imageBytes);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error converting file to base64: " + ex.Message);
+            }
+        }
 
         public static string CreateCid()
         {
